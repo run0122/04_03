b58b9f6100689dfa445c2915da0e6ba41ada6fc7
run0122 <run0122@naver.com>
2023-04-03 10:14:16 +0900

    347p
    millis

--- millis_practice.ino
+++ millis_practice.ino
@@ -8,18 +8,45 @@
 //   Serial.println(t_now);
 // }
 
-unsigned long t_prev = 0;
-const unsigned long t_delay = 1000;
+// 2.
+// unsigned long t_prev = 0;
+// const unsigned long t_delay = 1000;
+
+// void setup() {
+//   Serial.begin(115200);
+// }
+
+// void loop() {
+//   unsigned long t_now = millis();
+//   if (t_now - t_prev >= t_delay) {
+//     t_prev = t_now;
+
+//     Serial.println(t_now);
+//   }
+// }
+
+unsigned long t1_prev = 0;
+const unsigned long t1_delay = 1000;
+
+unsigned long t2_prev = 0;
+const unsigned long t2_delay = 500;
 
 void setup() {
   Serial.begin(115200);
 }
 
 void loop() {
-  unsigned long t_now = millis();
-  if (t_now - t_prev >= t_delay) {
-    t_prev = t_now;
+  unsigned long t1_now = millis();
+  if (t1_now - t1_prev >= t1_delay) {
+    t1_prev = t1_now;
 
-    Serial.println(t_now);
+    Serial.println("t1");
+  }
+
+  unsigned long t2_now = millis();
+  if (t2_now - t2_prev >= t2_delay) {
+    t2_prev = t2_now;
+
+    Serial.println("\tt2");
   }
 }
